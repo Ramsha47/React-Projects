a83ccf46c099a877cc371ec6df90920624b1c8d2
Ramsh47 <ramshaqaiser4@gmail.com>
2025-04-23 23:33:49 +0500

    quiz app learning

--- Quiz-app/src/components/QuestionTimer.jsx
+++ Quiz-app/src/components/QuestionTimer.jsx
@@ -7,17 +7,25 @@ export default function QuestionTimer({timeout , onTimeout}){
 
     useEffect(()=>{
         console.log('SETTING TIMEOUT')
-        setTimeout(onTimeout ,timeout)
+        const timer = setTimeout(onTimeout ,timeout)
+
+        return ()=>{
+            clearTimeout(timer)
+        }
     },[timeout,onTimeout])  // to make sure that this effect re executes if one of those dependencies changes
 
     // setTimeout(timeout,onTimeout)  //this timer would be recreated if we not use it in the useEffect
 
     // To make sure that this is not re executed  all the time but only when the dependecies chnge
     useEffect(()=>{
-        setInterval(()=>{
+        const interval= setInterval(()=>{
             console.log('SETTING INTERVAL')
             setRemainingTime((prevRemainingTime) => prevRemainingTime - 100)
         },100)
+
+        return ()=>{
+            clearInterval(interval)
+        }
     },[])
 
     
--- Quiz-app/src/components/Quiz.jsx
+++ Quiz-app/src/components/Quiz.jsx
@@ -2,32 +2,30 @@
   When user answer the question we want to move to next question -> useState
   Every question will come with multiple possible ans we want to store those answers ->useState
 */
-import { useState , useCallback } from "react"
+import { useState , useCallback} from "react"
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz(){
     // Why this state managing way is not optimal here ?? --> one of the state here is redundant
     // const [activeQuestionIndex , setActiveQuestionIndex] =useState(0)
+    const [userAnswers, setUserAnswers] = useState([]) // we need to store user selected answers but we could derive active index from this also
+    
+    const activeQuestionIndex =  userAnswers.length ;
+    //we have to evlaute whether the quiz is over or not?
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     /*
       Every time the quiz comp gets reevluated a new function of this recreated
     */
-    const handleSelectAns = useCallback( function handleSelectAns(selectedAns){
+      const handleSelectAns = useCallback( function handleSelectAns(selectedAns){
         setUserAnswers((prevUserAns)=>{
             return [...prevUserAns , selectedAns]  //also stroting prev selected ans for other questions
-        })
+        });
     },[])
-   
+
     const handleSkipAns = useCallback(() => handleSelectAns(null),[handleSelectAns]);
-
-    const [userAnswers, setUserAnswers] = useState([]) // we need to store user selected answers but we could derive active index from this also
-    const activeQuestionIndex = userAnswers.length
-    
-
-    //we have to evlaute whether the quiz is over or not?
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     if(quizIsComplete){
         return (
@@ -37,25 +35,26 @@ export default function Quiz(){
             </div>
         );
     }
-    // Shuffling answers
-    const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffleAnswers.sort(()=>Math.random -0.5);
-
+    
     return (
         <div id = "quiz">
-            <div id = "question">
-              <QuestionTimer  
-                timeout={10000} 
-                onTimeout={handleSkipAns}/>  
-              <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id= "answers">
-                    {shuffleAnswers.map((answer) =>(
-                        <li key = {answer} className="answer">
-                        <button onClick={()=> handleSelectAns(answer)}>{answer}</button>
-                    </li>
-                    ))}
-                </ul>
-          </div>
+            <Question 
+              key={activeQuestionIndex}
+              index={activeQuestionIndex}
+              onSelectAns={handleSelectAns}
+              onSkipAns={handleSkipAns}
+            />
         </div>
     )  
 }
+
+// Needs to split this component bcz this is getting bigger and complex
+
+/* when this component execute the ansState changes and make answers shuffle re execute
+   we only wanna make answers shuffle for once . As a React dev you should avoid using the useEffect usage bcs it 
+   happens relatively quickly that you use UseEffecr wrong .
+
+   You can use useRef Hook (you can use ref for managing values that are stored and manages independently from
+   the component function lifecycle to which they belong )
+
+*/
