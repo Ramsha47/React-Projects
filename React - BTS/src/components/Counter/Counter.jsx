619871dddbedbe073991b6246eff4add8b750b86
Ramsh47 <ramshaqaiser4@gmail.com>
2025-04-25 12:49:36 +0500

    optimization in react app

--- React - BTS/src/components/Counter/Counter.jsx
+++ React - BTS/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState , memo } from 'react';
+import { useState , memo , useCallback  , useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -29,17 +29,20 @@ function isPrime(number) {
 
 const Counter= memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(() =>isPrime(initialCount),[initialCount]); //directly being executed whenever this comp function chnge
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  /* these will be recreated everytime as an object in javascript and it will be a different object in memory
+     than before the last execution of counter comp fucntion */
+  // RECREATION here will be prevented with the special hook useCallback in conjuntion with memo()
+  const handleDecrement= useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  },[])
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  },[])
 
   return (
     <section className="counter">
@@ -73,3 +76,6 @@ export default Counter
    3. Dont use it on components where props will change frequently.
 
 */
+
+
+/* useCallback hook needed sometime in conjuntion with memo to avoid un neccassry re executions */
